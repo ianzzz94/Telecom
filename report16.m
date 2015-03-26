4c9211a2b2f72dfae08c8ba7c8d7a057641e070e
unknown <izhang@TUD204002.tudelft.net>
2015-03-26 12:32:36 +0100

    laatste labdag rip

--- report16.m
+++ report16.m
@@ -2,7 +2,7 @@ clear;
 close;
 clc;
 
-Fs = 8000;
+Fs = 33000;
 send_x = send_refsignal(3, Fs, 2);
 
 for i = 1:1:length(send_x),
@@ -13,6 +13,11 @@ end
 h = ch3(x, y);
 t = [1/length(x):1/length(x): 1];
 subplot(211);
-plot(t, x, t, y);
+plot(t, x, 'blue', t, y, 'green' );
 subplot(212);
-plot(abs(h));
+plot(h);
+
+%%Note to self: zoom in op eerste samples
+
+c = 340.29 %% speed of sound
+d = c/Fs
